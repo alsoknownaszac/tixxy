ef7288191a40e1b23e371d340ab8c9a17fe5f421
alsoknownaszac <mayo16collins@gmail.com>
2025-02-09 23:59:22 +0100

    guest mode - empty guest list screen

--- src/screens/PlannerMode/GuestList/components/GuestListCard.jsx
+++ src/screens/PlannerMode/GuestList/components/GuestListCard.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Pressable, View } from "react-native";
 import FontText from "../../../../reuseable/FontText";
 
 export default function GuestListCard({ navigation, item }) {
+  const [openList, setOpenList] = useState(false);
+
   return (
-    <Pressable onPress={() => {}}>
-      <View className="bg-[#F3F2F2] mt-[16px] rounded-[20px] p-6 flex flex-row items-center justify-center">
-        <View className="flex flex-row items-center gap-2">
-          {/* {item.icon} */}
-          <FontText className="font-trapMedium text-[16px] text-[#2A2B2A] leading-[20px]">
-            {item.title}
-          </FontText>
+    <Pressable
+      onPress={() => {
+        setOpenList(!openList);
+      }}
+    >
+      <View className="border-2 border-[#F3F2F2] mt-[16px] rounded-[20px] ">
+        <View className="p-[20px] flex flex-row items-center justify-center">
+          <View className="flex flex-row items-center gap-2">
+            {/* {item.icon} */}
+            <FontText className="font-trapMedium text-[16px] text-[#2A2B2A] leading-[20px]">
+              {item.title}
+            </FontText>
+          </View>
         </View>
+        {openList && <View className="h-[1.5px] bg-[#DAD8D8] w-4/5 mx-auto" />}
+
+        {openList && (
+          <View className="p-[20px] px-[12px]">
+            <FontText>
+              To share a link with your guest, you need to create a form
+              questionnaire that includes the details of your quests that you
+              need, for example their names, phone numbers etc. thereafter you
+              share the link to the people you want present, and they fill the
+              form to get their details saved.
+            </FontText>
+          </View>
+        )}
       </View>
     </Pressable>
   );
