9840ae49f1644af6d8bbd9673a62fef4594b4eb6
alsoknownaszac <mayo16collins@gmail.com>
2025-02-28 18:11:20 +0100

    payment screens

--- src/screens/Payment/index.jsx
+++ src/screens/Payment/index.jsx
@@ -16,6 +16,39 @@ import MoreBtn from "../../../assets/icons/more_icon.svg";
 import PaymentListCard from "./components/PaymentListCard";
 
 export default function Payment({ navigation }) {
+  const [guestTab] = useState([
+    {
+      title: "Share link with Guest",
+      info: "To share a link with your guest, you need to create a form questionnaire that includes the details of your quests that you need, for example their names, phone numbers etc. thereafter you share the link to the people you want present, and they fill the form to get their details saved.",
+      // icon: (
+      //   <SendIcon width={24} height={24} strokeWidth={1} stroke="#2A2B2A" />
+      // ),
+      btn_name: "Create Form",
+      btn_link: "CreateForm",
+      key: 1,
+    },
+    {
+      title: "Register Guest",
+      info: "Share link with guest Register guest To register guests, you need to manually enter the details of guests, for example their names or phone numbers. thereafter you send their invitations to them.",
+      // icon: (
+      //   <KeyboardIcon width={24} height={24} strokeWidth={1} stroke="#2A2B2A" />
+      // ),
+      btn_name: "Register Guest",
+      btn_link: "RegisterGuest",
+      key: 2,
+    },
+    {
+      title: "Import list",
+      info: "To Import guest list, you should create the guest list in a spread sheet or pdf in a table format and then upload.",
+      // icon: (
+      //   <ImportIcon width={24} height={24} strokeWidth={1} stroke="#2A2B2A" />
+      // ),
+      btn_name: "Import List",
+      btn_link: "ImportList",
+      key: 3,
+    },
+  ]);
+
   return (
     <View className="flex-1 bg-[#FCFCFC]">
       <View className="mb-[22] flex-initial rounded-b-[20px]">
