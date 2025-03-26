72d7804b5db76e321419091f4b75da3619af7449
alsoknownaszac <mayo16collins@gmail.com>
2025-03-26 12:27:23 +0100

    create guest list manually - screens

--- src/screens/PlannerMode/GuestList/RegisterGuest.jsx
+++ src/screens/PlannerMode/GuestList/RegisterGuest.jsx
@@ -36,62 +36,65 @@ export default function RegisterGuest({ navigation }) {
     <LayoutContainer>
       {guestList == 0 ? (
         <View className="flex-1">
-          <View className="mb-[22] flex-initial rounded-b-[20px]">
-            <View className="py-[20px] flex flex-row justify-between items-center ">
-              <View className="w-1/6">
-                <Pressable onPress={() => navigation.goBack()}>
-                  <BackArrow
-                    width={26}
-                    height={26}
-                    strokeWidth={1}
-                    stroke="#9A9898"
-                  />
-                </Pressable>
+          <View>
+            <View className="mb-[22] flex-initial rounded-b-[20px]">
+              <View className="py-[20px] flex flex-row justify-between items-center ">
+                <View className="w-1/6">
+                  <Pressable onPress={() => navigation.goBack()}>
+                    <BackArrow
+                      width={26}
+                      height={26}
+                      strokeWidth={1}
+                      stroke="#9A9898"
+                    />
+                  </Pressable>
+                </View>
+                <View className="w-4/6">
+                  <FontText className="font-chillaxMedium text-center text-[16px] leading-[24px] text-[#2A2B2A]">
+                    Create Guest List
+                  </FontText>
+                </View>
+                <View className="w-1/6"></View>
               </View>
-              <View className="w-4/6">
-                <FontText className="font-chillaxMedium text-center text-[16px] leading-[24px] text-[#2A2B2A]">
-                  Create Guest List
+            </View>
+            <View className="flex">
+              <View className="mt-[20] w-2/3 mx-auto">
+                <FontText className="font-chillaxNormal text-center font- text-[17px] leading-[24px] mb-[10] text-[#595959]">
+                  Select information to be filled in to create a guest
                 </FontText>
               </View>
-              <View className="w-1/6"></View>
             </View>
-          </View>
-
-          <View className="flex">
-            <View className="mt-[20] w-2/3 mx-auto">
-              <FontText className="font-chillaxNormal text-center font- text-[17px] leading-[24px] mb-[10] text-[#595959]">
-                Select information to be filled in to create a guest
-              </FontText>
-            </View>
-          </View>
-          <FlatList
-            vertical
-            showsHorizontalScrollIndicator={false}
-            keyExtractor={(item) => item.id}
-            data={listInfo}
-            renderItem={({ item, index }) => <Checklist item={item} />}
-          />
-          <View className="flex">
+            <FlatList
+              vertical
+              showsHorizontalScrollIndicator={false}
+              keyExtractor={(item) => item.id}
+              data={listInfo}
+              renderItem={({ item, index }) => <Checklist item={item} />}
+            />
             <TouchableOpacity onPress={() => setGuestList(1)}>
-              <View className="w-2/4 bg-[#F7F7F7] gap-x-[8px] p-[8px] rounded-[10px] flex flex-row justify-center items-center mt-[20]">
+              <View className="w-2/4 bg-[#F7F7F7] p-[8px] rounded-[10px] flex flex-row justify-center items-center mt-[20]">
                 <AntDesign name="plus" size={20} color="black" />
-                <FontText className="font-chillaxNormal text-center text-[16px] leading-[18px] text-[#595959]">
+                <FontText className="ml-[8] font-chillaxNormal text-center text-[16px] leading-[18px] text-[#595959]">
                   Add Input field
                 </FontText>
               </View>
             </TouchableOpacity>
           </View>
           <View
-            className="my-[16px]"
-            style={{ bottom: 0, right: 0, position: "absolute" }}
+            style={{
+              bottom: 0,
+              position: "absolute",
+              width: "100%",
+              padding: 16,
+            }}
           >
-            <TouchableOpacity onPress={() => setGuestList(1)}>
-              <AddEventIcon
-                width={95}
-                height={95}
-                strokeWidth={0.3}
-                fill="none"
-              />
+            <TouchableOpacity
+              onPress={() => setGuestList(1)}
+              className="py-[12] bg-[#7E62F0] w-full rounded-[100px] mt-[10] mb-[27] "
+            >
+              <FontText className="text-white text-center text-[18px] font-chillaxMedium leading-[150%]">
+                Proceed
+              </FontText>
             </TouchableOpacity>
           </View>
         </View>
@@ -180,7 +183,7 @@ function Checklist({ item }) {
   return (
     <View className="flex flex-row items-center justify-center">
       <CheckBox
-        style={{ flex: 1, padding: 10 }}
+        className="flex-1 py-[10]"
         onClick={() => {
           setSelection(!isChecked);
         }}
