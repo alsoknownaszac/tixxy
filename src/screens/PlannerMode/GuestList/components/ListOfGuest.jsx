91888701152eb4efaf3f0014eda4febb1fba47f9
alsoknownaszac <mayo16collins@gmail.com>
2025-04-01 02:09:31 +0100

    create guest list manually - screens

--- src/screens/PlannerMode/GuestList/components/ListOfGuest.jsx
+++ src/screens/PlannerMode/GuestList/components/ListOfGuest.jsx
@@ -64,3 +64,43 @@ export default function ListOfGuest() {
     </Fragment>
   );
 }
+
+const ContactItem = function ({ item, last }) {
+  return (
+    <View
+      key={item.id}
+      style={[
+        styles.container,
+        { borderBottomWidth: last ? 0 : StyleSheet.hairlineWidth },
+      ]}
+    >
+      <Image style={styles.avatar} source={{ uri: item.avatar }} />
+      <View style={styles.body}>
+        <Text style={styles.name}>{`${item.name}`}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    backgroundColor: "#FFF",
+    height: CONTACT_ITEM_HEIGHT,
+    padding: 15,
+    borderColor: "#E5E5E5",
+  },
+  avatar: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+  },
+  body: {
+    flex: 1,
+    marginLeft: 10,
+  },
+  name: {
+    fontSize: 16,
+    color: "#333",
+  },
+});
