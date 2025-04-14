a38d348a4f9af9eb159d2e6361001d702fd3c26c
alsoknownaszac <mayo16collins@gmail.com>
2025-04-14 17:12:24 +0100

    Listening to authentication state

---
+++ src/features/authentication/services/useAuthState.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import auth from "@react-native-firebase/auth";
+
+export function useAuthState() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return { loggedIn: false };
+  }
+
+  return { loggedIn: false, user };
+}
